ae9f85c07d1b28608285bd4bcf3e28a9fe9a0a2d
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-05-27 00:13:21 +0300

    header

--- layout/Header.js
+++ layout/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Link from 'next/link';
 
 function Header() {
     return (
-        <div>Header</div>
+        <header className='flex items-center justify-between bg-black p-10'>
+            <nav>
+                <ul className='flex space-x-4'>
+                    <li>
+                        <Link href="/" passHref legacyBehavior>
+                            <a className='text-blue-500 hover:text-blue-700'>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" passHref legacyBehavior>
+                            <a className='text-blue-500 hover:text-blue-700'>About Me</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" passHref legacyBehavior>
+                            <a className='text-blue-500 hover:text-blue-700'>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
