42888bb43213dd9eb7011c6c418006c19d30e9a3
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-05-27 00:36:23 +0300

    footer

--- layout/Footer.js
+++ layout/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className='items-center justify-between '>Footer</div>
+        </footer>
     )
 }
 
