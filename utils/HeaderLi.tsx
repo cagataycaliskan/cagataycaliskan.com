2f85f861064e3aa770eb83bb12b890679793ab8f
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-05-29 21:23:56 +0300

    ActiveLink düzenlendi

---
+++ utils/HeaderLi.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+function HeaderLi({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <li>
+      <Link href={href} passHref legacyBehavior>
+        <a
+          className={`text-white hover:text-orange-400 font-medium ${
+            pathname === href ? "active-link" : ""
+          }`}
+        >
+          {children}
+        </a>
+      </Link>
+    </li>
+  );
+}
+
+export default HeaderLi;
