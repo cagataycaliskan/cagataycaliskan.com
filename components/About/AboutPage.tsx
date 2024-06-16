d2b946bb99688781751df15cf517e5f3bcb1d86a
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-06-17 00:28:19 +0300

    About Page düzenlendi

--- components/About/AboutPage.tsx
+++ components/About/AboutPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <div className="container bg-white">About Page</div>
+    </>
+  );
 }
 
 export default AboutPage;
