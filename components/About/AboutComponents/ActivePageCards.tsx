62b5ca674ac63a914d77d5506c776032245a50d4
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-07-20 12:29:32 +0300

    Sertifikalar Eklendi

--- components/About/AboutComponents/ActivePageCards.tsx
+++ components/About/AboutComponents/ActivePageCards.tsx
@@ -3,17 +3,29 @@ import React from "react";
 import ReusableCard from "./ReusableCard";
 
 function ActivePageCards() {
-  const handleClick = (text: string) => {
-    console.log(text);
+  const handleClick = (url: string) => {
+    window.open(url, "_blank");
   };
 
   return (
     <>
       <div className="reusable-card-container">
-        <ReusableCard text="Deneme1" onClick={() => handleClick("Deneme1")} />
-        <ReusableCard text="Deneme2" onClick={() => handleClick("Deneme2")} />
-        <ReusableCard text="Deneme3" onClick={() => handleClick("Deneme3")} />
-        <ReusableCard text="Deneme4" onClick={() => handleClick("Deneme4")} />
+        <ReusableCard
+          text="The Ultimate React Course 2024: React, Redux & More"
+          onClick={() =>
+            handleClick(
+              "https://ude.my/UC-a32a2086-9fac-44da-90ba-147073bce8ae"
+            )
+          }
+        />
+        <ReusableCard
+          text="The Modern Javascript Bootcamp Course"
+          onClick={() =>
+            handleClick(
+              "https://www.udemy.com/certificate/UC-db2e4c83-0a2f-4089-af85-2eca28a10285/"
+            )
+          }
+        />
       </div>
     </>
   );
