7fdfbe59e08f537cf7539b859842c66f89bc5f07
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-07-20 12:29:10 +0300

    Contact page mail yazıldı ++ devam edilecek

--- components/Contact/ContactPage.tsx
+++ components/Contact/ContactPage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function ContactPage() {
   return (
     <>
-      <div className="container">Contact</div>
+      <div className="container text-orange-400">
+        cagataycaliskan98@gmail.com
+      </div>
     </>
   );
 }
