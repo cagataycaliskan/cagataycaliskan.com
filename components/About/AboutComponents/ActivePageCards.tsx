a830b492138471e7cdda536ed28ab513c801b0e6
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-07-20 15:42:53 +0300

    Sertifikalar map ile bastırıldı

--- components/About/AboutComponents/ActivePageCards.tsx
+++ components/About/AboutComponents/ActivePageCards.tsx
@@ -7,25 +7,27 @@ function ActivePageCards() {
     window.open(url, "_blank");
   };
 
+  const myCertificates = [
+    {
+      certificateName: "The Ultimate React Course 2024: React, Redux & More",
+      certificateUrl: "https://ude.my/UC-a32a2086-9fac-44da-90ba-147073bce8ae",
+    },
+    {
+      certificateName: "The Modern Javascript Bootcamp Course",
+      certificateUrl:
+        "https://www.udemy.com/certificate/UC-db2e4c83-0a2f-4089-af85-2eca28a10285/",
+    },
+  ];
+
   return (
     <>
       <div className="reusable-card-container">
-        <ReusableCard
-          text="The Ultimate React Course 2024: React, Redux & More"
-          onClick={() =>
-            handleClick(
-              "https://ude.my/UC-a32a2086-9fac-44da-90ba-147073bce8ae"
-            )
-          }
-        />
-        <ReusableCard
-          text="The Modern Javascript Bootcamp Course"
-          onClick={() =>
-            handleClick(
-              "https://www.udemy.com/certificate/UC-db2e4c83-0a2f-4089-af85-2eca28a10285/"
-            )
-          }
-        />
+        {myCertificates.map((certificate) => (
+          <ReusableCard
+            text={certificate.certificateName}
+            onClick={() => handleClick(certificate.certificateUrl)}
+          />
+        ))}
       </div>
     </>
   );
