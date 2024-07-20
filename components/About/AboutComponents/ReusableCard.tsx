014388b4698fa8f5113fca62e64f7f8fcd2b334e
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-07-20 17:17:30 +0300

    Modal card içine eklendi

--- components/About/AboutComponents/ReusableCard.tsx
+++ components/About/AboutComponents/ReusableCard.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 import "./AboutPageStyle.css";
 
 interface ReusableCardProps {
   text: string;
-  onClick: () => void;
+  imageUrl: string;
+  linkUrl: string;
 }
 
-const ReusableCard: React.FC<ReusableCardProps> = ({ text, onClick }) => {
+const ReusableCard: React.FC<ReusableCardProps> = ({
+  text,
+  imageUrl,
+  linkUrl,
+}) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleCardClick = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div className="reusable-card" onClick={onClick}>
-      {text}
-    </div>
+    <>
+      <div className="reusable-card" onClick={handleCardClick}>
+        {text}
+      </div>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          imageUrl={imageUrl}
+          linkUrl={linkUrl}
+        />
+      )}
+    </>
   );
 };
 
