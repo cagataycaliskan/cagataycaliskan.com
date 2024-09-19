ea99e0b552263e50c778529ad854fe8e836fae7f
Çağatay Çalışkan <cagataycaliskan98@gmail.com>
2024-09-19 17:44:57 +0300

    Yeni sertifika eklendi

--- src/components/About/AboutComponents/ActivePageCards.tsx
+++ src/components/About/AboutComponents/ActivePageCards.tsx
@@ -17,6 +17,13 @@ function ActivePageCards() {
       imageurl:
         "https://udemy-certificate.s3.amazonaws.com/image/UC-db2e4c83-0a2f-4089-af85-2eca28a10285.jpg?v=1679665277000",
     },
+    {
+      certificateName: "Go - The Complete Guide",
+      certificateUrl:
+        "https://www.udemy.com/certificate/UC-6a05c8fe-59fe-4808-8c71-24507ad425bb/",
+      imageurl:
+        "https://udemy-certificate.s3.amazonaws.com/image/UC-6a05c8fe-59fe-4808-8c71-24507ad425bb.jpg?v=1726752175000",
+    },
   ];
 
   return (
